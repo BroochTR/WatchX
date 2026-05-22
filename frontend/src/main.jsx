b73c333b66ee797a19b9e7d9a2312a13de0bd495
minhhitgub <hungminhnguyena1@gmail.com>
2026-05-22 23:25:00 +0700

    Revamp global CSS, simplify app mount, add icon

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// Mount the React app to the root element
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
-  );
-} else {
-  // Optionally log an error if root element is missing
-  console.error('Root element not found');
-}
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
